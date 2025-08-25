f4a4d5dc94f08521785f1831e0ec8e1d562fedc2
Adam Power <adamthedash2@gmail.com>
2025-08-25 21:56:34 +0100

    Completion keybinds

--- lua/plugins/load.lua
+++ lua/plugins/load.lua
@@ -519,7 +519,13 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
+        preset = 'none',
+        ['<C-e>'] = { 'select_and_accept', 'fallback' },
+        ['<Esc>'] = { 'cancel', 'fallback' },
+        ['<C-k>'] = { 'show_signature', 'fallback' },
+        ['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
+        ['<C-Space>'] = { 'show', 'show_documentation', 'hide_documentation' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
